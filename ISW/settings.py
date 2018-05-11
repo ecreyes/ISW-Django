99d449cd7c0bdd408acf08c06ddf7adbeee751f6
ecreyes <eduardo.reyeso.14@sansano.usm.cl>
2018-05-11 11:01:53 -0300

    se cambio el idioma de la plataforma a español y se configuro la base de datos

--- ISW/settings.py
+++ ISW/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'ISW.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'isw',
+        'USER': 'postgres',
+        'PASSWORD': '12345',
+        'HOST':'localhost',
+        'PORT': 5432,
     }
 }
 
@@ -103,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-mx'
 
 TIME_ZONE = 'UTC'
 
