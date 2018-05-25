f886fd84b3658e3f250121885f677c700634ae3a
ecreyes <ereyesoyarzo@gmail.com>
2018-05-25 18:45:18 -0400

    urls para acceder a el reset del password Issue #7

--- ISW/urls.py
+++ ISW/urls.py
@@ -15,11 +15,20 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from django.contrib.auth.views import login
+from django.contrib.auth.views import login,\
+        password_reset,password_reset_done,password_reset_confirm,password_reset_complete
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('trayectoria/',include('apps.trayectoria.urls',namespace='trayectoria')),
     path('usuario/',include('apps.usuario.urls',namespace='usuario')),
     path('',login,{'template_name':'index.html'},name='login'),
+    path('reset/password_reset',password_reset,{'template_name':'registration/password_reset_form.html',
+        'email_template_name':'registration/password_reset_email.html'},name='password_reset'),
+    path('reset/password_done',password_reset_done,
+        {'template_name':'registration/password_reset_done.html'},name='password_reset_done'),
+    path('reset/<uidb64>/<token>/',password_reset_confirm,
+        {'template_name':'registration/password_reset_confirm.html'},name='password_reset_confirm'),
+    path('reset/done',password_reset_complete,
+        {'template_name':'registration/password_reset_complete.html'},name='password_reset_complete'),
 ]
