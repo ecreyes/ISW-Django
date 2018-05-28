696f73676c348081192a79ccd04cff2ea1d5c1c5
ecreyes <ereyesoyarzo@gmail.com>
2018-05-28 15:52:22 -0400

    ahora se puede hacer una simulación de trayectoria de forma online, estuve probando con facebook no con las demas. Issue #20

---
+++ apps/trayectoria/online.py
@@ -0,0 +1,81 @@
+from yahoo_quote_download import yqd
+import numpy as np
+#Simulación de una trayectoria
+###INPUTS###
+#s_1: valor trayectoria actual
+#conta: contador de iteracion
+#d: numero de dias
+#r: tasa libre de riesgo
+#sigma: media aritmetica de valores de cierre en escala logaritmica
+#t: tiempo en años
+###OUPUTS: Ver en el codigo ###
+def trayectoria(s_1,conta,d,r,sigma,t):
+        epsilon = np.random.normal()
+        delta_s = r*s_1*t + sigma*s_1*epsilon*np.sqrt(t)
+        sn_1 = s_1 + delta_s #Nuevo valor de trayectoria
+        conta = conta + 1
+        if conta == d:
+                return sn_1 #Retornar a ultimo valor de trayectoria calculada
+        return trayectoria(sn_1,conta,d,r,sigma,t) #Si aun no termina el contador, seguir recursivamente
+
+#Simulacion de varias trayectorias
+###INPUTS###
+#nombre: nombre de la empresa
+#desde y hasta: fechas en formato 'AAAAMMDD' que representan un intervalo de tiempo para hacer la simulacion
+#i_anios: tiempo en años que se quieren estimar
+#i_tasa: valor de la tasa
+#i_precio: precio inicial
+#num_tray: numero de trayectorias a generar
+###OUTPUTS###
+#funcion: valor final de la simulacion
+#media_trayectorias: promedio/media de las trayectorias obtenidas
+#valores_trayectorias: lista de los valores de las trayectorias
+def simulacion(nombre,desde,hasta,i_anios,i_tasa,i_precio, num_tray):
+        #obtengo los datos de FB , desde-hasta
+        datos = yqd.load_yahoo_quote(nombre, desde, hasta)
+        #lista contiene todos los valores de cierre
+        valores_cierre = []
+        for i in datos:
+                if (i):
+                        lista = i.split(',')
+                        if(lista[4]!='Close'):
+                                valores_cierre.append(float(lista[4]))
+                                a = str(float(lista[4]))
+
+        #lista contiene todos los valores del ln(j/j-1)
+        valores_log = []
+        for j in range(1,len(valores_cierre)):
+                calculo = (np.log(valores_cierre[j]))/(np.log(valores_cierre[j-1]))
+                valores_log.append(calculo)
+
+        #valores a utilizar
+        trayectorias = num_tray
+        d = int(i_anios*360) #numero de dias
+        t = i_anios/1000 #tiempo en años
+        r = i_tasa *d/360 #tasa libre de riesgo
+        s = valores_cierre #lista que se utilizan para las simulaciones
+        RC = valores_log #lista que se utilizan para las simulaciones (valores logaritmicos)
+        precio = i_precio #precio estimado
+        sigma = np.std(valores_log)
+        #resultados obtenidos
+        valores_trayectorias = []
+        valores_max = []
+
+
+
+        #simulacion de todas las trayectorias requeridas
+
+        for i in range(1,trayectorias):
+                #simulacion de 1 trayectoria
+                sn_1 = trayectoria(s[len(s)-1],0,d,r,sigma,t)
+                valores_trayectorias.append(sn_1) #agrego ultimo valor de la trayectoria simulada
+                valores_max.append(np.maximum(sn_1-precio,0)) #agrego maximo entre 0 y la diferencia entre el ultimo valor de trayectoria simuladay el precio
+                #inicializo valores para futuras trayectorias.
+                sigma = np.std(valores_log)
+                RC = valores_log
+
+        esperanza = np.mean(valores_max)
+        funcion = np.exp(-i_tasa*i_anios)*esperanza
+        media_trayectorias = np.mean(valores_trayectorias)
+        return [funcion, media_trayectorias, valores_trayectorias]
+
--- apps/trayectoria/views.py
+++ apps/trayectoria/views.py
@@ -3,6 +3,7 @@ from .forms import TrayectoriaForm
 from django.http import HttpResponseRedirect
 from django.template import RequestContext
 from .models import Enterprise
+from apps.trayectoria.online import simulacion
 
 
 # Create your views here.
@@ -21,7 +22,28 @@ def index(request):
             opcion = form.cleaned_data['opcion']
             desde = form.cleaned_data['desde']
             hasta = form.cleaned_data['hasta']
-            return HttpResponseRedirect('/graficos/') #Redirigir a nueva pagina ,agregando los datos obtenidos del formulario como contexto
+            if(desde.day<10):
+                desde_dia = '0'+str(desde.day)
+            else:
+                desde_dia = str(desde.day)
+            if(desde.month<10):
+                desde_mes = '0'+str(desde.month)
+            else:
+                desde_mes = str(desde.month)
+            if(hasta.day<10):
+                hasta_dia = '0'+str(hasta.day)
+            else:
+                hasta_dia = str(hasta.day)
+            if(hasta.month<10):
+                hasta_mes = '0'+str(hasta.month)
+            else:
+                hasta_mes = str(hasta.month)
+            desde_string = str(desde.year)+desde_mes+desde_dia
+            hasta_string = str(hasta.year)+hasta_mes+hasta_dia
+            resultado = simulacion(simbol,desde_string,hasta_string,tiempo_años,tasa_de_interes,precio,100)
+
+            context ={'funcion':resultado[0],'promedio':resultado[1]}
+            return render(request,'trayectoria/resultado.html',context) #Redirigir a nueva pagina ,agregando los datos obtenidos del formulario como contexto
     else:
         form = TrayectoriaForm()
         context = {"form": form}
