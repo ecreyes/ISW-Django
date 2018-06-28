c596b363cc9a4ce3db38d27399c1c97056513fae
omuga <Obriel.muga.14@sansano.usm.cl>
2018-06-28 17:01:54 -0400

    Correccion de Errores en Vistas
    Se corrigieron errores en las vistas como:
    
    -Colores de Formularios
    -Se agregaron solo clases validas de bootstrap a los formularios
    -Se modificiaron archivos de formularios de django para utilizar
    bootstrap. #28

--- apps/formulario/forms.py
+++ apps/formulario/forms.py
@@ -15,13 +15,69 @@ YESTERDAY  = datetime.datetime.now() - datetime.timedelta(days = 1)
 YESTERDAY = YESTERDAY.strftime("%d/%m/%Y")
       
 class FormularioArchivoCsv(forms.Form):
-    symbol = forms.FileField()
-    tasa_de_interes = forms.FloatField()
-    tiempo_años = forms.IntegerField()
-    precio = forms.IntegerField()
-    tipo = forms.ChoiceField(choices=TIPO)
-    opcion = forms.ChoiceField(choices=OPCION)
-    desde = forms.DateField(initial=YESTERDAY)
-    hasta = forms.DateField(initial=datetime.date.today)
+    symbol = forms.FileField(widget=forms.FileInput(
+            attrs={
+                'style': 'border-color: green;',
+                'placeholder': 'Archivo CSV: ',
+                'class': 'form-control'
+            }
+        )
+    )  
+    tasa_de_interes = forms.FloatField(widget=forms.NumberInput(
+            attrs={
+                'style': 'border-color: green;',
+                'placeholder': 'Tasa de Interes',
+                'class': 'form-control'
+            }
+        )
+    )
+    tiempo_años = forms.IntegerField(widget=forms.NumberInput(
+            attrs={
+                'style': 'border-color: green;',
+                'placeholder': 'Tiempo en Años',
+                'class': 'form-control'
+            }
+        )
+    )
+    precio = forms.IntegerField(widget=forms.NumberInput(
+            attrs={
+                'style': 'border-color: green;',
+                'placeholder': 'Precio',
+                'class': 'form-control'
+            }
+        )
+    )
+    tipo = forms.ChoiceField(choices=TIPO,widget=forms.Select(
+            attrs={
+                'style': 'border-color: green;',
+                'placeholder': 'Tipo',
+                'class': 'form-control'
+            }
+        )
+    )
+    opcion = forms.ChoiceField(choices=OPCION,widget=forms.Select(
+            attrs={
+                'style': 'border-color: green;',
+                'placeholder': 'Opcion',
+                'class': 'form-control'
+            }
+        )
+    )
+    desde = forms.DateField(widget=forms.DateInput(
+            attrs={
+                'style': 'border-color: green;',
+                'placeholder': 'Desde --- Ex: 23/04/2016',
+                'class': 'form-control'
+            }
+        )
+    )
+    hasta = forms.DateField(widget=forms.DateInput(
+            attrs={
+                'style': 'border-color: green;',
+                'placeholder': 'Hasta --- Ex: 26/04/2016',
+                'class': 'form-control'
+            }
+        )
+    )
 
     
